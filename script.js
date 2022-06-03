36f73c47d9d49f66e3170c5ff34600090efcbe6c
Reese Sorrell <joreesesorrell@gmail.com>
2022-06-02 23:29:32 -0500

    Draw basic grid on the page.
    Create parent and children functions that will create a grid of divs
    inside of each other. Use CSS flexbox and background color to
    format and visualize the boxes.

---
+++ script.js
@@ -0,0 +1,24 @@
+function makeParentBox(num) {
+    const newDev = document.createElement('div');
+    const container = document.querySelector('.container');
+    newDev.classList.add('parent');
+    newDev.setAttribute('id', `row${num}`)
+    container.appendChild(newDev);
+}
+
+function makeChildBox(parent) {
+    const newDev = document.createElement('div');
+    newDev.classList.add('child');
+    parent.appendChild(newDev);
+}
+
+let length = 16;
+let height = 16;
+
+for (let i=0; i<length; i++) {
+    makeParentBox(i+1);
+    const box = document.querySelector(`#row${i+1}`);
+    for (let n=0; n<height; n++) {
+        makeChildBox(box);
+    }
+}
